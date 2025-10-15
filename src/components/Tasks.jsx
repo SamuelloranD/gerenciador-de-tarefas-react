d1144333154f9843cf08f3d553a63ae6536ff75c
Samuel Lorand <samuel.lorand@hotmail.com>
2025-10-15 17:43:11 -0300

    Check icon

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { Backpack, ChevronRightIcon, TrashIcon } from "lucide-react";
+import { Backpack, CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -22,7 +22,8 @@ function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
             {tasks.map((task) => (
                 <li key={task.id} className="flex gap-2">
 
-                    <button onClick={() => onTaskClick(task.id)} className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${task.isCompleted && "line-through"}`}>
+                    <button onClick={() => onTaskClick(task.id)} className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${task.isCompleted && "line-through"}`}>
+                        {task.isCompleted && <CheckIcon />}
                         {task.title}
                     </button>
 
